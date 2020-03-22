1580937fce532f60de506c31ff7bc608346929a1
Rotar Rares <rotarrares95@gmail.com>
2020-03-22 16:05:54 +0200

    Update AppTest.java
    Hacked into your repository

--- src/test/java/MaxPointsPatricipantsMV/AppTest.java
+++ src/test/java/MaxPointsPatricipantsMV/AppTest.java
@@ -28,15 +28,31 @@ public class AppTest
 
     @Test
     public void testAddStudentService() {
-
         Validator<Student> studentValidator = new StudentValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\Users\\Rares\\Documents\\VVSS\\MaxPointsPatricipantsMV\\src\\test\\java\\MaxPointsPatricipantsMV\\studenti2.xml");
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\studenti2.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\teme2.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\note2.xml");
 
-        Student student = new Student("1", "nume", 916);
-        fileRepository1.save(student);
-        assertTrue(fileRepository1.findOne(student.getID()).equals(student));
-        fileRepository1.delete(student.getID());
+        Service studentService = new Service(fileRepository1,fileRepository2,fileRepository3);
+        studentService.saveTema("2", "De la mic la mare inaintam tare",31,1);
+        studentService.saveStudent("1", "nume", 916);
+        studentService.saveNota("1","2",32,20,"Formidable! :)");
 
-
+        assertTrue(studentService.updateStudent("1","Ghencea Mihai",936)==1);
     }
+
+    @Test
+    public void testAddStudentRepo() {
+
+    Validator<Student> studentValidator = new StudentValidator();
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\studenti2.xml");
+
+    Student student = new Student("1", "nume", 916);
+        fileRepository1.save(student);
+    assertTrue(fileRepository1.findOne(student.getID()).equals(student));
+        fileRepository1.delete(student.getID());
+    }
+
 }
