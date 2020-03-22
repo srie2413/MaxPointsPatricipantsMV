ccce1aec912162c6c1bcbceae616c9f9188d462e
srie2413 <srie2413@scs.ubbcluj.ro>
2020-03-22 19:18:48 +0200

    refactor unit tests

--- src/test/java/MaxPointsPatricipantsMV/BBT/StudentServiceTest.java
+++ src/test/java/MaxPointsPatricipantsMV/BBT/StudentServiceTest.java
@@ -1,8 +1,15 @@
 package MaxPointsPatricipantsMV.BBT;
 
+import MaxPointsPatricipantsMV.domain.Nota;
 import MaxPointsPatricipantsMV.domain.Student;
+import MaxPointsPatricipantsMV.domain.Tema;
+import MaxPointsPatricipantsMV.repository.NotaXMLRepository;
 import MaxPointsPatricipantsMV.repository.StudentXMLRepository;
+import MaxPointsPatricipantsMV.repository.TemaXMLRepository;
+import MaxPointsPatricipantsMV.service.Service;
+import MaxPointsPatricipantsMV.validation.NotaValidator;
 import MaxPointsPatricipantsMV.validation.StudentValidator;
+import MaxPointsPatricipantsMV.validation.TemaValidator;
 import MaxPointsPatricipantsMV.validation.Validator;
 import org.junit.Test;
 
@@ -11,19 +18,18 @@ import static org.junit.Assert.assertTrue;
 public class StudentServiceTest {
 
     @Test
-    @Test
     public void testAddStudentService() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\studenti2.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\teme2.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "C:\\\\Users\\rotar\\Desktop\\Projects\\vvss\\Lab2\\src\\test\\java\\MaxPointsParticipants\\note2.xml");
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "C:\\Users\\Rares\\Documents\\VVSS\\MaxPointsPatricipantsMV\\studenti2.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "C:\\Users\\Rares\\Documents\\VVSS\\MaxPointsPatricipantsMV\\teme2.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "C:\\Users\\Rares\\Documents\\VVSS\\MaxPointsPatricipantsMV\\note2.xml");
 
         Service studentService = new Service(fileRepository1,fileRepository2,fileRepository3);
-        studentService.saveTema("2", "De la mic la mare inaintam tare",31,1);
-        studentService.saveStudent("1", "nume", 916);
-        studentService.saveNota("1","2",32,20,"Formidable! :)");
+        studentService.saveTema("2", "tema",31,1);
+        studentService.saveStudent("1", "nume", 936);
+        studentService.saveNota("1","2",32,20,"");
 
         assertTrue(studentService.updateStudent("1","Ghencea Mihai",936)==1);
     }
