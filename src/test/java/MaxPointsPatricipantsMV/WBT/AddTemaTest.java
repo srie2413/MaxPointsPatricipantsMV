422512ecc640db52ed862b6b6b677c552851c205
Serban <rsie2408@scs.ubbcluj.ro>
2020-04-17 02:08:16 +0300

    salut

--- src/test/java/MaxPointsPatricipantsMV/WBT/AddTemaTest.java
+++ src/test/java/MaxPointsPatricipantsMV/WBT/AddTemaTest.java
@@ -4,14 +4,9 @@ package MaxPointsPatricipantsMV.WBT;
 import MaxPointsPatricipantsMV.domain.Nota;
 import MaxPointsPatricipantsMV.domain.Student;
 import MaxPointsPatricipantsMV.domain.Tema;
-import MaxPointsPatricipantsMV.repository.NotaXMLRepository;
-import MaxPointsPatricipantsMV.repository.StudentXMLRepository;
-import MaxPointsPatricipantsMV.repository.TemaXMLRepository;
+import MaxPointsPatricipantsMV.repository.*;
 import MaxPointsPatricipantsMV.service.Service;
-import MaxPointsPatricipantsMV.validation.NotaValidator;
-import MaxPointsPatricipantsMV.validation.StudentValidator;
-import MaxPointsPatricipantsMV.validation.TemaValidator;
-import MaxPointsPatricipantsMV.validation.Validator;
+import MaxPointsPatricipantsMV.validation.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,7 +25,9 @@ public class AddTemaTest {
     TemaXMLRepository fileRepository2;
     NotaXMLRepository fileRepository3;
 
+
     Service temaService;
+    CRUDRepository repo;
 
     @Before
     public void init() {
@@ -76,17 +73,82 @@ public class AddTemaTest {
 
     @Test
     public void testAddAssignmentExistingId() {
-
-        Assert.assertEquals(1, temaService.saveTema("1", "tema_existingID", 5, 5));
-        Assert.assertEquals(0, temaService.saveTema("1", "tema_existingID2", 6, 6));
+       temaService.saveTema("1", "tema_existingID", 5, 5);
+        Assert.assertEquals(1, temaService.saveTema("1", "tema_existingID2", 6, 6));
     }
 
     @Test
     public void testAddAssignmentIdValid() {
 
-        Assert.assertEquals(1, temaService.saveTema("4", "tema_Id", 5, 5));
-        temaService.deleteTema("4");
+        Assert.assertEquals(0, temaService.saveTema("2", "tema_Id", 5, 5));
     }
+    @Test
+    public void testSaveAssignmentEntityValid()
+    {
+        Tema t = new Tema("3","tema_id3",10,5);
+        Assert.assertEquals(t, fileRepository2.save(t));
+    }
+    @Test
+    public void testSaveAssignmentEntityExist()
+    {
+        Tema t = new Tema("3","tema_id3",10,5);
+        fileRepository2.save(t);
+        Tema t1 = new Tema("3","tema_id33",10,5);
+        Assert.assertEquals(null, fileRepository2.save(t1));
+    }
+    @Test
+    public void testAddAssignmentIdNotValid()
+    {
+        Assert.assertEquals(1, temaService.saveTema("", "tema_Id", 5, 5));
+    }
+
+    @Test
+    public void testAddAssignmentIdIsNull()
+    {
+        Assert.assertEquals(1, temaService.saveTema(null, "tema_Id", 5, 5));
+    }
+    @Test
+    public void testAddAssignmentDescisNotValid()
+    {
+        Assert.assertEquals(1, temaService.saveTema("1", "", 5, 5));
+    }
+
+    @Test
+    public void testAddAssignmentDescIsNull()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", null, 5, 5));
+    }
+    @Test
+    public void testAddAssigmentStartLineTooSmall()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 5, 0));
+    }
+    @Test
+    public void testAddAssigmentStartLineTooBig()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 5, 15));
+    }
+    @Test
+    public void testAddAssigmentStartLineNotValid()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 5, 6));
+    }
+    @Test
+    public void testAddAssigmentDeadLineTooSmall()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 0, 5));
+    }
+    @Test
+    public void testAddAssigmentDeadLineTooBig()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 15, 5));
+    }
+    @Test
+    public void testAddAssigmentDeadLineNotValid()
+    {
+        Assert.assertEquals(1, temaService.saveTema("6", "desc", 4, 5));
+    }
+
 
 }
 
